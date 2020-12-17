2633dcf4a012c3928eff87ce7e92d431f4db946d
Vikram Sastry <vikramsastry@yahoo.com>
2020-12-16 17:26:17 -0800

    Creating query endpoint to connect to dbs

--- microservice/main.go
+++ microservice/main.go
@@ -7,9 +7,9 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello World")
 
 	r := gin.Default()
+
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "TFLite microservice endpoints",
@@ -17,14 +17,26 @@ func main() {
 	})
 
   r.POST("/queryImage", queryImage)
+	r.GET("/queryConfidenceInterval")
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
 
 func queryImage(c *gin.Context) {
 
 	var err error
+	f, uploadedFile, err := c.Request.FormFile("file")
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": err.Error(),
+			"error":   true,
+		})
+		return ""
+	}
+	defer f.close()
 
   key:= cloudbucket.HandleUploadtoCloudBucket(c)
+
 	ctx := appengine.NewContext(c.Request)
+
 
 }
