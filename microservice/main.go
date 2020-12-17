d0d44f1b67a7893292e55cc0675f3c079b9b20a4
Vikram Sastry <vikramsastry@yahoo.com>
2020-12-17 10:54:01 -0800

    Updated queryImage endpoint

--- microservice/main.go
+++ microservice/main.go
@@ -7,17 +7,21 @@ import (
 )
 
 func main() {
-
+  //gin server
 	r := gin.Default()
 
+  //simple test endpoint
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "TFLite microservice endpoints",
 		})
 	})
 
+  //query image inference results
   r.POST("/queryImage", queryImage)
-	r.GET("/queryConfidenceInterval")
+
+  //get confidence interval
+	r.POST("/queryConfidenceInterval")
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
 
@@ -29,8 +33,7 @@ func queryImage(c *gin.Context) {
 		c.JSON(http.StatusInternalServerError, gin.H{
 			"message": err.Error(),
 			"error":   true,
-		})
-		return ""
+		});
 	}
 	defer f.close()
 
@@ -38,5 +41,54 @@ func queryImage(c *gin.Context) {
 
 	ctx := appengine.NewContext(c.Request)
 
+	queryCF := CallCloudFunction(f, key)
+}
 
+func CallCloudFunction(f File, key string) {
+	var err error
+
+	#Put in config
+  url = "https://us-west2-vikcraft.cloudfunctions.net/function-tfl-1"
+	req, err := http.NewRequest("POST", url, body)
+
+	fi, err := file.Stat()
+	if err != nil {
+		return nil, err
+	}
+
+	body := &bytes.Buffer{}
+	writer := multipart.NewWriter(body)
+	part, err := writer.CreateFormFile(filetype, fi.Name())
+
+	fileContents, err := ioutil.ReadAll(f)
+
+	if err != nil {
+		return nil, err
+	}
+	part.Write(fileContents)
+
+	writer.Close()
+	request, err := http.NewRequest("POST", url, body)
+
+	if err != nil {
+			log.Fatal(err)
+	}
+
+	request.Header.Add("Content-Type", writer.FormDataContentType())
+	client := &http.Client{}
+
+	response, err := client.Do(request)
+
+	if err != nil {
+			log.Fatal(err)
+	}
+	defer response.Body.Close()
+
+	content, err := ioutil.ReadAll(response.Body)
+
+	if err != nil {
+			log.Fatal(err)
+	}
+
+	return content
 }
